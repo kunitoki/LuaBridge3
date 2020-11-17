f407df2bbf5a70e7e2594915fbcceb63163a2962
kunitoki <kunitoki@gmail.com>
2020-11-17 23:34:29 +0100

    Sanity check we can compile LuaBridge on objc

---
+++ Tests/Source/ObjCTests.mm
@@ -0,0 +1,8 @@
+// https://github.com/kunitoki/LuaBridge
+// Copyright 2020, Lucio Asnaghi
+// SPDX-License-Identifier: MIT
+
+#include "Lua/LuaLibrary.h"
+
+// This is here just to sanity check we can compile on objc
+#include "LuaBridge/LuaBridge.h"
