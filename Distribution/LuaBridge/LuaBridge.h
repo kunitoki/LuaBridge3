f8a9d951ad444d874787511b52f2c7d227dfd3a5
kunitoki <kunitoki@users.noreply.github.com>
2022-12-27 21:44:24 +0000

    Update amalgamation file

--- Distribution/LuaBridge/LuaBridge.h
+++ Distribution/LuaBridge/LuaBridge.h
@@ -5156,6 +5156,18 @@ struct is_const_member_function_pointer<R (T::*)(Args...) const>
     static constexpr bool value = true;
 };
 
+template <class T, class R, class... Args>
+struct is_const_member_function_pointer<R (T::*)(Args...) noexcept>
+{
+    static constexpr bool value = false;
+};
+
+template <class T, class R, class... Args>
+struct is_const_member_function_pointer<R (T::*)(Args...) const noexcept>
+{
+    static constexpr bool value = true;
+};
+
 template <class T>
 inline static constexpr bool is_const_member_function_pointer_v = is_const_member_function_pointer<T>::value;
 
@@ -5992,6 +6004,15 @@ inline void push_function(lua_State* L, ReturnType (*fp)(Params...))
     lua_pushcclosure_x(L, &invoke_proxy_function<FnType>, 1);
 }
 
+template <class ReturnType, class... Params>
+inline void push_function(lua_State* L, ReturnType (*fp)(Params...) noexcept)
+{
+    using FnType = decltype(fp);
+
+    lua_pushlightuserdata(L, reinterpret_cast<void*>(fp));
+    lua_pushcclosure_x(L, &invoke_proxy_function<FnType>, 1);
+}
+
 template <class F, class = std::enable_if<is_callable_v<F> && !std::is_pointer_v<F> && !std::is_member_function_pointer_v<F>>>
 inline void push_function(lua_State* L, F&& f)
 {
@@ -6015,7 +6036,25 @@ void push_member_function(lua_State* L, ReturnType (*fp)(T*, Params...))
 }
 
 template <class T, class ReturnType, class... Params>
+void push_member_function(lua_State* L, ReturnType (*fp)(T*, Params...) noexcept)
+{
+    using FnType = decltype(fp);
+
+    lua_pushlightuserdata(L, reinterpret_cast<void*>(fp));
+    lua_pushcclosure_x(L, &invoke_proxy_function<FnType>, 1);
+}
+
+template <class T, class ReturnType, class... Params>
 void push_member_function(lua_State* L, ReturnType (*fp)(const T*, Params...))
+{
+    using FnType = decltype(fp);
+
+    lua_pushlightuserdata(L, reinterpret_cast<void*>(fp));
+    lua_pushcclosure_x(L, &invoke_proxy_function<FnType>, 1);
+}
+
+template <class T, class ReturnType, class... Params>
+void push_member_function(lua_State* L, ReturnType (*fp)(const T*, Params...) noexcept)
 {
     using FnType = decltype(fp);
 
@@ -6048,7 +6087,29 @@ void push_member_function(lua_State* L, ReturnType (U::*mfp)(Params...))
 }
 
 template <class T, class U, class ReturnType, class... Params>
+void push_member_function(lua_State* L, ReturnType (U::*mfp)(Params...) noexcept)
+{
+    static_assert(std::is_same_v<T, U> || std::is_base_of_v<U, T>);
+
+    using F = decltype(mfp);
+
+    new (lua_newuserdata_x<F>(L, sizeof(F))) F(mfp);
+    lua_pushcclosure_x(L, &invoke_member_function<F, T>, 1);
+}
+
+template <class T, class U, class ReturnType, class... Params>
 void push_member_function(lua_State* L, ReturnType (U::*mfp)(Params...) const)
+{
+    static_assert(std::is_same_v<T, U> || std::is_base_of_v<U, T>);
+
+    using F = decltype(mfp);
+
+    new (lua_newuserdata_x<F>(L, sizeof(F))) F(mfp);
+    lua_pushcclosure_x(L, &detail::invoke_const_member_function<F, T>, 1);
+}
+
+template <class T, class U, class ReturnType, class... Params>
+void push_member_function(lua_State* L, ReturnType (U::*mfp)(Params...) const noexcept)
 {
     static_assert(std::is_same_v<T, U> || std::is_base_of_v<U, T>);
 
@@ -6288,7 +6349,7 @@ protected:
     };
 
     LuaRefBase(lua_State* L)
-        : m_L(main_thread(L))
+        : m_L(L)
     {
     }
 
@@ -7846,6 +7907,32 @@ class Namespace : public detail::Registrar
             return *this;
         }
 
+        template <class U>
+        Class<T>& addStaticProperty(const char* name, U (*get)() noexcept, void (*set)(U) noexcept = nullptr)
+        {
+            assert(name != nullptr);
+            assertStackState(); 
+
+            lua_pushlightuserdata(L, reinterpret_cast<void*>(get)); 
+            lua_pushcclosure_x(L, &detail::invoke_proxy_function<U (*)() noexcept>, 1); 
+            detail::add_property_getter(L, name, -2); 
+
+            if (set != nullptr)
+            {
+                lua_pushlightuserdata(L, reinterpret_cast<void*>(set)); 
+                lua_pushcclosure_x(L, &detail::invoke_proxy_function<void (*)(U) noexcept>, 1); 
+            }
+            else
+            {
+                lua_pushstring(L, name); 
+                lua_pushcclosure_x(L, &detail::read_only_error, 1); 
+            }
+
+            detail::add_property_setter(L, name, -2); 
+
+            return *this;
+        }
+
         template <class Getter, class = std::enable_if_t<!std::is_pointer_v<Getter>>>
         Class<T>& addStaticProperty(const char* name, Getter get)
         {
@@ -7981,6 +8068,31 @@ class Namespace : public detail::Registrar
         }
 
         template <class TG, class TS = TG>
+        Class<T>& addProperty(const char* name, TG (T::*get)() const noexcept, void (T::*set)(TS) noexcept = nullptr)
+        {
+            using GetType = TG (T::*)() const noexcept;
+            using SetType = void (T::*)(TS) noexcept;
+
+            assert(name != nullptr);
+            assertStackState(); 
+
+            new (lua_newuserdata_x<GetType>(L, sizeof(GetType))) GetType(get); 
+            lua_pushcclosure_x(L, &detail::invoke_const_member_function<GetType, T>, 1); 
+            lua_pushvalue(L, -1); 
+            detail::add_property_getter(L, name, -5); 
+            detail::add_property_getter(L, name, -3); 
+
+            if (set != nullptr)
+            {
+                new (lua_newuserdata_x<SetType>(L, sizeof(SetType))) SetType(set); 
+                lua_pushcclosure_x(L, &detail::invoke_member_function<SetType, T>, 1); 
+                detail::add_property_setter(L, name, -3); 
+            }
+
+            return *this;
+        }
+
+        template <class TG, class TS = TG>
         Class<T>& addProperty(const char* name, TG (T::*get)(lua_State*) const, void (T::*set)(TS, lua_State*) = nullptr)
         {
             using GetType = TG (T::*)(lua_State*) const;
@@ -8006,7 +8118,32 @@ class Namespace : public detail::Registrar
         }
 
         template <class TG, class TS = TG>
-        Class<T>& addProperty(const char* name, TG (*get)(T const*), void (*set)(T*, TS) = nullptr)
+        Class<T>& addProperty(const char* name, TG (T::*get)(lua_State*) const noexcept, void (T::*set)(TS, lua_State*) noexcept = nullptr)
+        {
+            using GetType = TG (T::*)(lua_State*) const noexcept;
+            using SetType = void (T::*)(TS, lua_State*) noexcept;
+
+            assert(name != nullptr);
+            assertStackState(); 
+
+            new (lua_newuserdata_x<GetType>(L, sizeof(GetType))) GetType(get); 
+            lua_pushcclosure_x(L, &detail::invoke_const_member_function<GetType, T>, 1); 
+            lua_pushvalue(L, -1); 
+            detail::add_property_getter(L, name, -5); 
+            detail::add_property_getter(L, name, -3); 
+
+            if (set != nullptr)
+            {
+                new (lua_newuserdata_x<SetType>(L, sizeof(SetType))) SetType(set); 
+                lua_pushcclosure_x(L, &detail::invoke_member_function<SetType, T>, 1); 
+                detail::add_property_setter(L, name, -3); 
+            }
+
+            return *this;
+        }
+
+        template <class TG, class TS = TG>
+        Class<T>& addProperty(const char* name, TG (*get)(const T*), void (*set)(T*, TS) = nullptr)
         {
             assert(name != nullptr);
             assertStackState(); 
@@ -8021,6 +8158,28 @@ class Namespace : public detail::Registrar
             {
                 lua_pushlightuserdata( L, reinterpret_cast<void*>(set)); 
                 lua_pushcclosure_x(L, &detail::invoke_proxy_function<void (*)(T*, TS)>, 1); 
+                detail::add_property_setter(L, name, -3); 
+            }
+
+            return *this;
+        }
+
+        template <class TG, class TS = TG>
+        Class<T>& addProperty(const char* name, TG (*get)(const T*) noexcept, void (*set)(T*, TS) noexcept = nullptr)
+        {
+            assert(name != nullptr);
+            assertStackState(); 
+
+            lua_pushlightuserdata(L, reinterpret_cast<void*>(get)); 
+            lua_pushcclosure_x(L, &detail::invoke_proxy_function<TG (*)(const T*) noexcept>, 1); 
+            lua_pushvalue(L, -1); 
+            detail::add_property_getter(L, name, -5); 
+            detail::add_property_getter(L, name, -3); 
+
+            if (set != nullptr)
+            {
+                lua_pushlightuserdata( L, reinterpret_cast<void*>(set)); 
+                lua_pushcclosure_x(L, &detail::invoke_proxy_function<void (*)(T*, TS) noexcept>, 1); 
                 detail::add_property_setter(L, name, -3); 
             }
 
@@ -8685,6 +8844,39 @@ public:
         {
             lua_pushlightuserdata(L, reinterpret_cast<void*>(set)); 
             lua_pushcclosure_x(L, &detail::invoke_proxy_function<void (*)(TS)>, 1);
+        }
+        else
+        {
+            lua_pushstring(L, name);
+            lua_pushcclosure_x(L, &detail::read_only_error, 1);
+        }
+
+        detail::add_property_setter(L, name, -2);
+
+        return *this;
+    }
+
+    template <class TG, class TS = TG>
+    Namespace& addProperty(const char* name, TG (*get)() noexcept, void (*set)(TS) noexcept = nullptr)
+    {
+        if (m_stackSize == 1)
+        {
+            throw_or_assert<std::logic_error>("addProperty() called on global namespace");
+
+            return *this;
+        }
+
+        assert(name != nullptr);
+        assert(lua_istable(L, -1)); 
+
+        lua_pushlightuserdata(L, reinterpret_cast<void*>(get)); 
+        lua_pushcclosure_x(L, &detail::invoke_proxy_function<TG (*)() noexcept>, 1); 
+        detail::add_property_getter(L, name, -2);
+
+        if (set != nullptr)
+        {
+            lua_pushlightuserdata(L, reinterpret_cast<void*>(set)); 
+            lua_pushcclosure_x(L, &detail::invoke_proxy_function<void (*)(TS) noexcept>, 1);
         }
         else
         {
