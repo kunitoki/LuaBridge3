75b15efc42e6926ca3f52f2c6d477750261e778c
kunitoki <kunitoki@gmail.com>
2023-03-24 00:24:54 +0100

    More testing

--- Tests/Source/ClassTests.cpp
+++ Tests/Source/ClassTests.cpp
@@ -2032,8 +2032,7 @@ TEST_F(ClassMetaMethods, __newindex)
 
     luabridge::setGlobal(L, &t, "t");
 
-    runLua("t.a = 1\n"
-           "t ['b'] = 2");
+    runLua("t.a = 1; t['b'] = 2");
 
     ASSERT_EQ((std::map<std::string, int>{{"a", 1}, {"b", 2}}), t.map);
 }
@@ -2061,12 +2060,12 @@ TEST_F(ClassMetaMethods, metamethodsShouldNotBePartOfClassInstances)
         .endClass();
 
 #if LUABRIDGE_HAS_EXCEPTIONS
-    ASSERT_ANY_THROW(runLua("local x = Int(1); x.__gc()"));
-    ASSERT_ANY_THROW(runLua("local x = Int(1); x:__gc()"));
-    ASSERT_ANY_THROW(runLua("local x = Int(1); x.__index()"));
-    ASSERT_ANY_THROW(runLua("local x = Int(1); x:__index()"));
-    ASSERT_ANY_THROW(runLua("local x = Int(1); x.__newindex()"));
-    ASSERT_ANY_THROW(runLua("local x = Int(1); x:__newindex()"));
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x.__gc()"));
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x:__gc()"));
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x.__index()"));
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x:__index()"));
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x.__newindex()"));
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x:__newindex()"));
     EXPECT_TRUE(runLua("local x = Int(1); result = x.__gc"));
     EXPECT_TRUE(result().isNil());
     EXPECT_TRUE(runLua("local x = Int(1); result = x.__index"));
@@ -2086,6 +2085,26 @@ TEST_F(ClassMetaMethods, metamethodsShouldNotBePartOfClassInstances)
     EXPECT_TRUE(result().isNil());
     EXPECT_TRUE(runLua("local x = Int(1); result = x.__newindex"));
     EXPECT_TRUE(result().isNil());
+#endif
+}
+
+TEST_F(ClassMetaMethods, metamethodsShouldNotBeWritable)
+{
+    using Int = Class<int, EmptyBase>;
+
+    luabridge::getGlobalNamespace(L)
+        .beginClass<Int>("Int")
+            .addConstructor<void (*)(int)>()
+        .endClass();
+
+#if LUABRIDGE_HAS_EXCEPTIONS
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x.__gc = function() end"));
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x.__index = function() end"));
+    EXPECT_ANY_THROW(runLua("local x = Int(1); x.__newindex = function() end"));
+#else
+    EXPECT_FALSE(runLua("local x = Int(1); x.__gc = function() end"));
+    EXPECT_FALSE(runLua("local x = Int(1); x.__index = function() end"));
+    EXPECT_FALSE(runLua("local x = Int(1); x.__newindex = function() end"));
 #endif
 }
 
